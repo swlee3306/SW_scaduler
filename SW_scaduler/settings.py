3292f5d2d2d74f8ed7217567b79f42b64c079d73
swlee <swlee3306@gmail.com>
2020-08-23 16:13:27 +0900

    db 변경

--- SW_scaduler/settings.py
+++ SW_scaduler/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'SW_scaduler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'NAME': 'mysql-db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
