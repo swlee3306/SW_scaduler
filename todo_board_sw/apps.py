885cc2ae30c0b8590c49406ace487046d146c2cd
swlee <swlee3306@gmail.com>
2020-08-30 14:27:46 +0900

    todo_vserion_1

---
+++ todo_board_sw/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TodoBoardSwConfig(AppConfig):
+    name = 'todo_board_sw'
---
+++ todo_main_sw/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TodoMainSwConfig(AppConfig):
+    name = 'todo_main_sw'
