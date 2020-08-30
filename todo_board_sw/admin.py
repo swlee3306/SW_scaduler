17a4a9a6214e820516bb61d627403cad9ca8fa9e
swlee <swlee3306@gmail.com>
2020-08-30 22:35:49 +0900

    todo_version_5

--- todo_board_sw/admin.py
+++ todo_board_sw/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Todo_list
 
 # Register your models here.
+
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ('no', 'pcode' , 'user_id','title', 'content','is_complete')
+
+admin.site.register(Todo_list, TodoListAdmin)
+
